205032f67f909b77da2623e096ed2d4aa91c8823
Carlos Oliveira <21.00781-0@maua.br>
2024-06-01 21:24:09 -0300

    feat: implementação do segundo microsserviço

---
+++ backend/auth-service/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { initializeApp } = require('firebase/app');
+const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } = require('firebase/auth');
+const axios = require('axios');  // Importando o Axios para enviar eventos para o Buser de Eventos
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtsNBgEVJ1NJ5GqX3PCvOYSzb-CTRLDaI",
+  authDomain: "futdle.firebaseapp.com",
+  projectId: "futdle",
+  storageBucket: "futdle.appspot.com",
+  messagingSenderId: "754585551605",
+  appId: "1:754585551605:web:31d87a95a62c14e810bbbc",
+  measurementId: "G-WC9N2M4DB9"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      res.status(200).send(userCredential.user);
+    })
+    .catch(error => {
+      res.status(400).send({ error: error.message });
+    });
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password } = req.body;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      // Send event to Event Bus
+      axios.post('http://localhost:3003/events', {
+        type: 'Usuario Criado',
+        data: { id: userCredential.user.uid, email: userCredential.user.email }
+      }).catch((err) => {
+        console.log('Erro enviando evento pro Buser de Eventos', err.message);
+      });
+
+      res.status(201).send(userCredential.user);
+    })
+    .catch(error => {
+      res.status(400).send({ error: error.message });
+    });
+});
+
+app.post('/logout', (req, res) => {
+  signOut(auth)
+    .then(() => {
+      res.status(200).send({ message: 'Usuário deslogado com sucesso' });
+    })
+    .catch(error => {
+      res.status(400).send({ error: error.message });
+    });
+});
+
+app.post('/events', (req, res) => {
+  console.log('Evento recebido:', req.body.type);
+  res.send({});
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Serviço de Autenticação rodando na porta: ${PORT}`);
+});
