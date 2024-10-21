4186a10282fd7ebd718adda35eb2a7a0d7e983c8
cesarfkb <cesarfkb12@gmail.com>
2024-10-21 20:06:57 -0300

    feat: inicio da implementação com o microsserviço no backend

--- frontend_flutter/lib/pages/register_page.dart
+++ frontend_flutter/lib/pages/register_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_flutter/pages/home_page.dart';
+import 'package:http/http.dart' as http;
 
 class RegisterPage extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   RegisterPage({super.key});
+
+  Future<bool> _register() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    var response = await http.post(
+      Uri.parse('http://localhost:3001/register'),
+      body: {
+        'email': email,
+        'password': password,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +33,11 @@ class RegisterPage extends StatelessWidget {
         leading: IconButton(
           icon: const Icon(Icons.home),
           onPressed: () {
-            Navigator.pushNamed(context, '/');
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const HomePage(isLoggedIn: false)));
+            // Navigator.pushNamed(context, '/');
           },
         ),
         title: const Text('Registrar'),
@@ -44,9 +68,18 @@ class RegisterPage extends StatelessWidget {
             const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
-                String email = _emailController.text;
-                String password = _passwordController.text;
-                print('Email: $email, Senha: $password');
+                _register().then((value) {
+                  if (value) {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const HomePage(isLoggedIn: true)));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Erro ao registrar')));
+                  }
+                });
               },
               child: const Text('Registrar'),
             ),
