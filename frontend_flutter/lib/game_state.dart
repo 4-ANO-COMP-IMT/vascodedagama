f0ad876be317b7a2aaf389550b236ba417236d5f
Carlos Oliveira <21.00781-0@maua.br>
2024-10-28 23:55:19 -0300

    feat: lista de sugestão de palpite

--- frontend_flutter/lib/game_state.dart
+++ frontend_flutter/lib/game_state.dart
@@ -7,28 +7,74 @@ class GameState extends ChangeNotifier {
   int score = 0;
   int highScore = 0;
   List<PlayerGuess> guessHistory = [];
+  Map<String, dynamic>? guessedPlayer;
+  List<Map<String, dynamic>> suggestions = []; // Lista para sugestões de jogadores
+  bool isGuessCorrect = false; // Variável de estado para último palpite
 
-  GameState({required this.appProvider});
+  GameState({required this.appProvider}){
+    highScore = appProvider.highScore;
+    score = appProvider.score;
 
-  /// Carrega um novo jogador secreto usando o `AppProvider`
-  Future<void> loadNewSecretPlayer() async {
-    await appProvider.loadNewSecretPlayer();
+    // Adiciona o listener apenas uma vez
+    appProvider.addListener(_updateScoreFromAppProvider);
+  }
+
+   // Atualiza o score a partir do AppProvider
+  void _updateScoreFromAppProvider() {
+    score = appProvider.score;
     notifyListeners();
   }
 
-  /// Define o jogador secreto usando o `AppProvider`
-  void initializeSecretPlayer() {
+  @override
+  void dispose() {
+    appProvider.removeListener(_updateScoreFromAppProvider);
+    super.dispose();
+  }
+
+  /// Carrega um novo jogador secreto usando o AppProvider
+  Future<void> loadNewSecretPlayer() async {
+    await appProvider.loadNewSecretPlayer();
     if (appProvider.secretPlayer != null) {
-      setSecretPlayer(appProvider.secretPlayer!);
+      resetGame();
+      notifyListeners();
     }
   }
 
-  /// Define o jogador secreto no estado do jogo
-  void setSecretPlayer(Map<String, dynamic> playerData) {
-    guessHistory.clear();
-    attempts = 0;
-    score = 0;
-    notifyListeners();
+  /// Verifica se o palpite está correto
+  void checkGuess(guessedPlayerId, secretPlayerId) {
+    if (appProvider.secretPlayer != null && guessedPlayer != null) {
+      if (guessedPlayerId == secretPlayerId) { // Compare IDs
+        print('Acertou!');
+        isGuessCorrect = true; // Define como verdadeiro se acertou
+        updateScore(true); // Atualiza o score se acertar
+        attempts = 0; // Reseta o número de tentativas
+      } else {
+        isGuessCorrect = false; // Define como falso se errou
+        incrementAttempts();
+        // Adiciona o palpite ao histórico
+        final guessedPlayerData = appProvider.players.firstWhere((player) => player['id'] == guessedPlayerId);
+        addToGuessHistory(
+          Player(
+            guessedPlayerData['id'],
+            guessedPlayerData['name'],
+            guessedPlayerData['team'],
+            guessedPlayerData['foot'],
+            guessedPlayerData['height'],
+            guessedPlayerData['position'],
+            guessedPlayerData['age'],
+            guessedPlayerData['league'],
+            guessedPlayerData['country']
+          ), 
+          true
+        );
+        notifyListeners();
+        if (attempts >= 5) {
+          resetGame(); // Reseta após 5 tentativas erradas
+        }
+      }
+      this.guessedPlayer = guessedPlayer; // Atribui o jogador adivinhado ao GameState
+      notifyListeners();
+    }
   }
 
   /// Incrementa o número de tentativas
@@ -37,15 +83,14 @@ class GameState extends ChangeNotifier {
     notifyListeners();
   }
 
-  /// Atualiza a pontuação, reseta se incorreta
+  /// Atualiza a pontuação e o highScore
   void updateScore(bool isCorrect) {
     if (isCorrect) {
       score++;
-      if (score > highScore) {
+      if (score > appProvider.highScore) {
         highScore = score;
+        appProvider.saveHighScore(highScore); // Salva o novo highScore
       }
-    } else {
-      score = 0;
     }
     notifyListeners();
   }
@@ -56,11 +101,13 @@ class GameState extends ChangeNotifier {
     notifyListeners();
   }
 
-  /// Reinicia o jogo, mantendo apenas o `highScore`
+  /// Reinicia o jogo, mantendo apenas o highScore
   void resetGame() {
     attempts = 0;
-    score = 0;
     guessHistory.clear();
+    guessedPlayer = null; // Limpa o jogador adivinhado
+    appProvider.resetScore(); // Reseta o score diretamente no AppProvider
+    score = appProvider.score; // Atualiza o score local após o reset
     notifyListeners();
   }
 }
@@ -73,7 +120,16 @@ class PlayerGuess {
 }
 
 class Player {
+  final int id;
   final String name;
+  final String team; 
+  final String foot; 
+  final int height; 
+  final String icon = 'https://via.placeholder.com/150';
+  final String position;
+  final int age;
+  final String league;
+  final String country;
 
-  Player(this.name);
+  Player(this.id, this.name, this.team, this.foot, this.height, this.position, this.age, this.league, this.country);
 }
